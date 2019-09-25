9ec9a08fbc0c200f53a788ebed99561ff10f6542
Tim Berners-Lee <timbl+git@w3.org>
2019-09-25 18:43:31 -0400

    my first electron app

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # data-kitchen
 The solid databrowser technology as a stand-alone electron app
+
+## To run
+
+```
+npm install
+npm start
+```
+
+The goal of this project is to provide a kitchen where the user is cook, and
+the power user is chef.
+
+Live your life in a read-write world of lined data.
+
+Use all your solid pods, including your local files as your zeroth pod.
+bring in all the stuff in your life shared by friends and colleagues.
+Bring in public data like dbpedia and wikidata and the LOD cloud.
---
+++ renderer.js
@@ -0,0 +1,48 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+console.log('renderer.js')
+console.log('renderer.js panes ' + panes)
+
+// From browse.html in forms playground:
+// const panes = require('mashlib')
+const UI = panes.UI
+const $rdf = UI.rdf
+const dom = document
+$rdf.Fetcher.crossSiteProxyTemplate = self.origin + '/xss?uri={uri}';
+var uri = window.location.href;
+window.document.title = 'Data browser: ' + uri;
+var kb = UI.store;
+var outliner = panes.getOutliner(dom)
+
+function go ( event ) {
+  let uri = $rdf.uri.join(uriField.value, window.location.href)
+  console.log("User field " + uriField.value)
+  console.log("User requests " + uri)
+
+  const params = new URLSearchParams(location.search)
+  params.set('uri', uri);
+  window.history.replaceState({}, '', `${location.pathname}?${params}`);
+
+  var subject = kb.sym(uri);
+  // UI.widgets.makeDraggable(icon, subject) // beware many handlers piling up
+  outliner.GotoSubject(subject, true, undefined, true, undefined);
+}
+
+const uriField = dom.getElementById('uriField')
+const goButton = dom.getElementById('goButton')
+uriField.addEventListener('keyup', function (e) {
+  if (e.keyCode === 13) {
+    go(e)
+  }
+}, false)
+
+goButton.addEventListener('click', go, false);
+let initial = new URLSearchParams(self.location.search).get("uri")
+if (initial) {
+  uriField.value = initial
+  go()
+} else {
+  console.log('ready for user input')
+}
