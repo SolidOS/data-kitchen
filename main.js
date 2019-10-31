ec1668bf67e9b9c630e0a9237b05862396e47d08
Ruben Verborgh <ruben@verborgh.org>
2019-10-31 22:30:11 +0100

    Fix popup windows.
    Fixes https://github.com/solid/data-kitchen/issues/1

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function createWindow () {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nativeWindowOpen: true,
       nodeIntegration: true
     }
   })
