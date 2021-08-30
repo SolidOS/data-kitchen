e9239f88693bf69aa77e012e7232c0d8a43bdeb2
jeff-zucker <dubzed@gmail.com>
2021-08-30 13:36:42 -0700

    latest mashlib

--- ServerRoot/common/kitchen.js
+++ ServerRoot/common/kitchen.js
@@ -3,6 +3,8 @@ var port =3100;
 var host = `http://localhost:${port}`;
 
 window.SolidAppContext = {
+  noAuth : host,
+  webId : host + "/LocalKitchenUser/profile/card#me",
   app : host,
   webid : host + "/LocalKitchenUser/profile/card#me",
   scroll : 130 // for eyeFocus, should be height of top banner
